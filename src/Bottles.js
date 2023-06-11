64de4c9d222d0c868e603234af49fccb78be8bb0
mab_gray <m2bbie@gmail.com>
2023-06-11 07:28:30 -0500

    Fixed bug

--- src/Bottles.js
+++ src/Bottles.js
@@ -1,61 +1,40 @@
-
 import React, { useState } from 'react';
 import './Bottles.css';
+import spices from './spicesData';
 
+export function Bottles() {
+  const [spiceList, setSpiceList] = useState(spices);
 
-const spices = [
-    {name: "black salt", id:1, gotIt: false},
-    {name: "paprika", id:2, gotIt: false },
-     {name: "pepper", id:3, gotIt: false}
-];
+  const manageClick = (spiceId) => {
+    const updatedSpiceList = spiceList.map(spice => {
+      if (spice.id === spiceId) {
+        return {
+          ...spice,
+          gotIt: !spice.gotIt
+        };
+      }
+      return spice;
+    });
 
+    setSpiceList(updatedSpiceList);
+  };
 
- export function Bottles(){
+  const renderedSpiceList = spiceList.map(spice => (
+    <div
+      key={spice.id}
+      className={spice.gotIt ? "bottleColor" : "bottle"}
+      onClick={() => {
+        manageClick(spice.id);
+      }}
+    >
+      <p className="bottle-text">{spice.name}</p>
+    </div>
+  ));
 
-   const [spiceList, setSpiceList] = useState(spices);
-
-
-   const manageClick = (spiceId) => {
-      const updatedSpiceList = spiceList.map(spice => {
-         if(spice.id === spiceId){
-            return {
-              ...spice,
-              gotIt: !spice.gotIt
-            };
-         }
-         return spice;
-      })
-     
-      setSpiceList(updatedSpiceList);
-   }
-
-  
-
-    const renderedSpiceList = spiceList.map(spice => 
-      <div key={spice.id}
-         className={spice.gotIt ? "bottleColor" : "bottle" }
-          
-        
-         onClick={()=> {
-            manageClick(spice.id)
-         }}
-
-
-      >
-         <p className="bottle-text">{spice.name}</p> 
-      </div>
-      
-      );
-
-
-      return (
-         
-         <div  className="bottle-container">
-         {renderedSpiceList}
-         </div>
-         
-
-        
-    );
-    
+  return (
+    <div className="bottle-container">
+      {renderedSpiceList}
+    </div>
+  );
 }
+
