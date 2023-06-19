40341a2c80d16e234046f4b463437aab3115f8f0
mab_gray <m2bbie@gmail.com>
2023-06-19 18:43:28 -0500

    Persist and usestate nothing works

--- src/Bottles.js
+++ src/Bottles.js
@@ -1,40 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Bottles.css';
 import spices from './spicesData';
 
 export default function Bottles() {
-  const [spiceList, setSpiceList] = useState(spices);
+  const [spiceList, setSpiceList] = useState([]);
 
+  // Load state from localStorage on component mount
+   useEffect(() => {
+    const storedState = localStorage.getItem('spiceList');
+    if (storedState) {
+      setSpiceList(JSON.parse(storedState));
+    } else {
+      setSpiceList(spices);
+    }
+  }, []); 
+
+  // Update state and save to localStorage when changes occur
   const manageClick = (spiceId) => {
-    const updatedSpiceList = spiceList.map(spice => {
+    const updatedSpiceList = spiceList.map((spice) => {
       if (spice.id === spiceId) {
         return {
           ...spice,
-          gotIt: !spice.gotIt
+          gotIt: !spice.gotIt,
         };
       }
       return spice;
     });
 
     setSpiceList(updatedSpiceList);
-  };
+    localStorage.setItem('spiceList', JSON.stringify(updatedSpiceList));
+  }; 
 
-  const renderedSpiceList = spiceList.map(spice => (
+  const renderedSpiceList = spiceList.map((spice) => (
     <div
       key={spice.id}
-      className= { spice.gotIt ? "bottleColor" : "bottle" }
-      onClick={() => {
-        manageClick(spice.id);
-      }}
+      className={spice.gotIt ? 'bottleColor' : 'bottle'}
+      onClick={() => manageClick(spice.id)}
     >
       <p className="bottle-text">{spice.name}</p>
     </div>
   ));
 
-  return (
-    <div className="bottle-container">
-      {renderedSpiceList}
-    </div>
-  );
-}
+  return <div className="bottle-container">{renderedSpiceList}</div>;
 
+  }
+
+  
--- src/spicesData.js
+++ src/spicesData.js
@@ -1,8 +1,63 @@
-const spices = [
-    { name: "black salt", id: 1, gotIt: false },
-    { name: "paprika", id: 2, gotIt: false },
-    { name: "pepper", id: 3, gotIt: false }
+
+  
+  const spices = [
+    { id: 1, name: 'Allspice', gotIt: false },
+    { id: 2, name: 'Basil', gotIt: false },
+    { id: 3, name: 'Bay leaf', gotIt: false },
+    { id: 4, name: 'Caraway', gotIt: false },
+    { id: 5, name: 'Cardamom', gotIt: false },
+    { id: 6, name: 'Cayenne pepper', gotIt: false },
+    { id: 7, name: 'Chili powder', gotIt: false },
+    { id: 8, name: 'Cinnamon', gotIt: false },
+    { id: 9, name: 'Cloves', gotIt: false },
+    { id: 10, name: 'Coriander', gotIt: false },
+    { id: 11, name: 'Cumin', gotIt: false },
+    { id: 12, name: 'Curry powder', gotIt: false },
+    { id: 13, name: 'Dill', gotIt: false },
+    { id: 14, name: 'Fennel seeds', gotIt: false },
+    { id: 15, name: 'Garlic powder', gotIt: false },
+    { id: 16, name: 'Ginger', gotIt: false },
+    { id: 17, name: 'Lemon pepper', gotIt: false },
+    { id: 18, name: 'Mace', gotIt: false },
+    { id: 19, name: 'Marjoram', gotIt: false },
+    { id: 20, name: 'Mustard seeds', gotIt: false },
+    { id: 21, name: 'Nutmeg', gotIt: false },
+    { id: 22, name: 'Oregano', gotIt: false },
+    { id: 23, name: 'Paprika', gotIt: false },
+    { id: 24, name: 'Parsley', gotIt: false },
+    { id: 25, name: 'Pepper', gotIt: false },
+    { id: 26, name: 'Poppy seeds', gotIt: false },
+    { id: 27, name: 'Rosemary', gotIt: false },
+    { id: 28, name: 'Saffron', gotIt: false },
+    { id: 29, name: 'Sage', gotIt: false },
+    { id: 30, name: 'Salt', gotIt: false },
+    { id: 31, name: 'Sesame seeds', gotIt: false },
+    { id: 32, name: 'Tarragon', gotIt: false },
+    { id: 33, name: 'Thyme', gotIt: false },
+    { id: 34, name: 'Turmeric', gotIt: false },
+    { id: 35, name: 'Vanilla', gotIt: false },
+    { id: 36, name: 'White pepper', gotIt: false },
+    { id: 37, name: 'Anise', gotIt: false },
+    { id: 38, name: 'Arrowroot', gotIt: false },
+    { id: 39, name: 'Bouillon', gotIt: false },
+    { id: 40, name: 'Chives', gotIt: false },
+    { id: 41, name: 'Cilantro', gotIt: false },
+    { id: 42, name: 'Curry leaves', gotIt: false },
+    { id: 43, name: 'Fenugreek', gotIt: false },
+    { id: 44, name: 'Juniper berries', gotIt: false },
+    { id: 45, name: 'Lovage', gotIt: false },
+    { id: 46, name: 'Mint', gotIt: false },
+    { id: 47, name: 'Peppermint', gotIt: false },
+    { id: 48, name: 'Savory', gotIt: false },
+    { id: 49, name: 'Star anise', gotIt: false },
+    { id: 50, name: 'Tamarind', gotIt: false }
   ];
   
+  
+  
+
+
+
+
   export default spices;
   
